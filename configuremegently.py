d6948e5b5fa43f6116047b75dc97a215611c1d14
marcin@flondra <consommepanchi@gmail.com>
2017-02-05 16:21:18 +0100

    m

--- configuremegently.py
+++ configuremegently.py
@@ -19,27 +19,33 @@ def parse_output(proc):
     return None
 
 
+def pull_and_update():
+    subprocess.call(["git", "pull"], cwd="dotfiles")
+    with open("paths.txt", "r") as paths, open("vars.json", "r") as varsfile:
+        files = paths.read().split("\n")[:-1]
+        varsdict = json.loads(varsfile.read())
+        for filename in files:
+            with open("dotfiles/" + filename.split("/")[-1]) as dotfile:
+                template = Template(dotfile.read())
+                localfilecontent = template.render(varsdict)
+                with open(os.path.expanduser(filename), "w") as localfile:
+                    localfile.write(localfilecontent)
+            print "written " + filename
+
+
 if __name__ == "__main__":
     proc = subprocess.Popen(
         ['git', 'status'], cwd="dotfiles/", stdout=subprocess.PIPE)
     sys.stdout.flush()
     git_status = parse_output(proc)
     if git_status == "clean":
-        subprocess.call(["git", "pull"], cwd="dotfiles")
-        with open("paths.txt", "r") as paths, open("vars.json", "r") as varsfile:
-            files = paths.read().split("\n")[:-1]
-            varsdict = json.loads(varsfile.read())
-            for filename in files:
-                with open("dotfiles/" + filename.split("/")[-1]) as dotfile:
-                    template = Template(dotfile.read())
-                    localfilecontent = template.render(varsdict)
-                    with open(os.path.expanduser(filename), "w") as localfile:
-                        localfile.write(localfilecontent)
-                print "written " + filename
+        pull_and_update()
     elif git_status == "dirty":
         subprocess.call(["git", "add", "--all"], cwd="dotfiles/")
-        subprocess.call(["git", "commit", "-mconfiguredgently"], cwd="dotfiles/")
+        subprocess.call(
+            ["git", "commit", "-mconfiguredgently"], cwd="dotfiles/")
         subprocess.call(["git", "push", "origin", "master"], cwd="dotfiles/")
+        pull_and_update()
     elif git_status == "unpushed":
         subprocess.call(["git", "push", "origin", "master"], cwd="dotfiles/")
     else:
